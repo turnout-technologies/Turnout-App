8d96f4f693ef43d637f526725e7ab6e868de6463
Tyler Fox <tyler_fox@alumni.brown.edu>
2020-03-22 00:52:53 -0400

    update feedback to send to slack+sentry

--- src/APIClient.js
+++ src/APIClient.js
@@ -4,6 +4,8 @@ import * as Sentry from 'sentry-expo';
 
 import * as Env from './Environment';
 
+const turnoutAPIInstance = axios.create();
+
 const successHandler = (response) => {
   	return response
 }
@@ -13,21 +15,21 @@ const errorHandler = (error) => {
   	return Promise.reject({ ...error })
 }
 
-axios.defaults.baseURL = Env.getAPIUrl();
-axios.interceptors.request.use(async config => {
+turnoutAPIInstance.defaults.baseURL = Env.getAPIUrl();
+turnoutAPIInstance.interceptors.request.use(async config => {
 	config.headers["Authorization"] = "Bearer " + await firebase.auth().currentUser.getIdToken();
 	return config
 }, (error) => {
  return Promise.reject(error)
 })
 
-axios.interceptors.response.use(
+turnoutAPIInstance.interceptors.response.use(
   response => successHandler(response),
   error => errorHandler(error)
 )
 
 export function addUser(name, email, avatarURL, pushToken) {
-	return axios.post("/users", {
+	return turnoutAPIInstance.post("/users", {
 	    name: name,
 	    email: email,
 	    avatarURL: avatarURL,
@@ -36,38 +38,28 @@ export function addUser(name, email, avatarURL, pushToken) {
 }
 
 export function getUser(uid) {
-	return axios.get("/users/"+uid);
+	return turnoutAPIInstance.get("/users/"+uid);
 }
 
 export function putPushToken(uid, token) {
-	return axios.put("/users/"+uid+"/push-token", {pushToken: token});
+	return turnoutAPIInstance.put("/users/"+uid+"/push-token", {pushToken: token});
 }
 
 export function getLeaderboard() {
-	return axios.get("/users/leaderboard");
+	return turnoutAPIInstance.get("/users/leaderboard");
 }
 
 export function getBallotToday() {
-	return axios.get("/ballots/today");
+	return turnoutAPIInstance.get("/ballots/today");
 }
 
 export function submitBallot(ballotId, userId, questionResponse) {
-	return axios.post("/ballots/today/"+ballotId, {
+	return turnoutAPIInstance.post("/ballots/today/"+ballotId, {
 	    userId: userId,
 	    response: questionResponse
 	 });
 }
 
 export function getLatestBallotResults() {
-	return axios.get("/ballots/latest/results");
-}
-
-export function sendFeedback(type, message, filename, name, email) {
-	return axios.post("/contact", {
-	    type: type,
-	    message: message,
-	    filename: filename,
-	    name: name,
-	    email: email
-	 });
+	return turnoutAPIInstance.get("/ballots/latest/results");
 }
