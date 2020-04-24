175beefe48021f886991e025819fdc0fd0d26e42
Tyler Fox <tyler_fox@alumni.brown.edu>
2020-04-24 08:15:43 -0400

    Update PollStatusCountdown.js

--- src/components/PollStatusCountdown.js
+++ src/components/PollStatusCountdown.js
@@ -69,7 +69,7 @@ export default class PollStatusCountdown extends Component {
   setPollState() {
     var pollsOpen = false;
     var curMoment = moment();
-    //var curMoment = moment.tz("2020-04-22 18:59:50", "America/New_York");
+    //var curMoment = moment.tz("2020-04-23 18:59:50", "America/New_York");
     var pollsOpenTimeEastern = moment.tz({y:curMoment.year(), M:curMoment.month(), date:curMoment.date(), h:18, m:0}, "America/New_York");
     var pollsCloseTimeEastern = moment.tz({y:curMoment.year(), M:curMoment.month(), date:curMoment.date(), h:22, m:0}, "America/New_York");
     if (curMoment <= pollsOpenTimeEastern) {
